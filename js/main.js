0549291b74898320ae38bd46764e1c69c96b1304
Daffyta <daffy.dine9@gmail.com>
2017-04-04 14:50:07 -0500

    Prueba git colaborativo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 var boton = document.getElementById('boton');
 boton.addEventListener("click",eliminarBoton);
 
+
+//este boton me vacia las celdas y las colorea con un nuevo backgroundColor
 function eliminarBoton() {
   this.style.display = "none";
   document.getElementById('celdaVacia').style.backgroundColor = "#00bfff";
 }
+
+// var celdasV1=document.getElementById("celdasVacias1");
+// var celdasV2=document.getElementById("celdasVacias2");
+// var numeritos=document.getElementsByClassName("numero");
+//
+// celdasV1.addEventListener("click",colorCeldas);
+// celdasV2.addEventListener("click",colorCeldas);
+// function colorCeldas() {
+//        var padre=this.parentNode;
+//    this.style.display = "none";
+//    padre.style.backgroundColor = "#00BFFF "
+// }
